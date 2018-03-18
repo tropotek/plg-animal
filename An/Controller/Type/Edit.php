c81274a065161b5b72a0cf1e673e9c7fe3c8b392
mifsudm <mifsudm@unimelb.edu.au>
2018-03-19 10:37:23 +1100

    ~Auto: Minor Code Updates - 252s-dev

--- An/Controller/Type/Edit.php
+++ An/Controller/Type/Edit.php
@@ -66,8 +66,8 @@ class Edit extends AdminEditIface
         $this->form->addField(new Field\Textarea('description'));
         $this->form->addField(new Field\Textarea('notes'));
 
-        $this->form->addField(new Event\Button('update', array($this, 'doSubmit')));
-        $this->form->addField(new Event\Button('save', array($this, 'doSubmit')));
+        $this->form->addField(new Event\Submit('update', array($this, 'doSubmit')));
+        $this->form->addField(new Event\Submit('save', array($this, 'doSubmit')));
         $this->form->addField(new Event\Link('cancel', \Uni\Ui\Crumbs::getInstance()->getBackUrl()));
 
     }
